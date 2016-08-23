78c4635be52eef010c421350bcd943f24adacfa9
Arthur van Kruining <arthurvk@xs4all.nl>
2016-08-23 16:00:28 +0200

    Add completed_at ability.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,12 @@ before_action :find_item, only: [:show, :edit, :update, :destroy]
 		redirect_to root_path
 	end
 
+	def complete
+		@item = Item.find(params[:id])
+		@item.update_attribute(:completed_at, Time.now)
+		redirect_to root_path
+	end
+
 	private
 
 	  def item_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :items
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def completed?
+  	!completed_at.blank?
+  end
+
 end
