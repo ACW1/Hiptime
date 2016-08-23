e6f5d2cef3443059c44450a9f4945869b7806b70
Arthur van Kruining <arthurvk@xs4all.nl>
2016-08-23 13:52:15 +0200

    Add user_id to Items table and Associations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822131507) do
+ActiveRecord::Schema.define(version: 20160823111822) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
